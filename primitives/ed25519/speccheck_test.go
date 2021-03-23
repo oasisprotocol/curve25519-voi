796f8d41d113534369a2abdeae37cf590769e3bf
Yawning Angel <yawning@schwanenlied.me>
2021-03-23 11:06:53 +0000

    primitives/ed25519: Add support for verification with precomputation
    This opts to duplicate the signature verification code instead of
    converting normal public keys to the precomputed representation on the
    fly because doing so incurs heap allocations, while the existing serial
    verification routine does not.

--- primitives/ed25519/speccheck_test.go
+++ primitives/ed25519/speccheck_test.go
@@ -45,6 +45,14 @@ import (
 	"testing"
 )
 
+type verificationImpl int
+
+const (
+	implVanilla verificationImpl = iota
+	implBatch
+	implExpanded
+)
+
 var speccheckExpectedResults = []bool{
 	false, // 0: small order A, small order R
 	false, // 1: small order A, mixed order R
@@ -138,17 +146,17 @@ func (v *speccheckTestVector) toComponents() ([]byte, PublicKey, []byte, error) 
 	return msg, pk, sig, nil
 }
 
-func (v *speccheckTestVector) Run(t *testing.T, isBatch bool, opts *Options) bool {
+func (v *speccheckTestVector) Run(t *testing.T, impl verificationImpl, opts *Options) bool {
 	msg, pk, sig, err := v.toComponents()
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	var sigOk bool
-	switch isBatch {
-	case false:
+	switch impl {
+	case implVanilla:
 		sigOk = VerifyWithOptions(pk, msg, sig, opts)
-	case true:
+	case implBatch:
 		v := NewBatchVerifier()
 		for i := 0; i < testBatchSize; i++ {
 			v.AddWithOptions(pk, msg, sig, opts)
@@ -161,6 +169,14 @@ func (v *speccheckTestVector) Run(t *testing.T, isBatch bool, opts *Options) boo
 				t.Fatalf("sigOk != valid[%d]", i)
 			}
 		}
+	case implExpanded:
+		expPub, err := NewExpandedPublicKey(pk)
+		if err != nil {
+			t.Fatalf("NewExpandedPublicKey: %v", err)
+		}
+		sigOk = ExpandedVerifyWithOptions(expPub, msg, sig, opts)
+	default:
+		return false
 	}
 
 	return sigOk
@@ -189,12 +205,17 @@ func TestSpeccheck(t *testing.T) {
 		for idx, tc := range testVectors {
 			expected := expectedResults[idx]
 			t.Run(fmt.Sprintf("%s/%d", n, idx), func(t *testing.T) {
-				if sigOk := tc.Run(t, false, opts); sigOk != expected {
+				if sigOk := tc.Run(t, implVanilla, opts); sigOk != expected {
 					t.Fatalf("behavior mismatch: %v (expected %v)", sigOk, expected)
 				}
 			})
 			t.Run(fmt.Sprintf("%s_Batch/%d", n, idx), func(t *testing.T) {
-				if sigOk := tc.Run(t, true, opts); sigOk != expected {
+				if sigOk := tc.Run(t, implBatch, opts); sigOk != expected {
+					t.Fatalf("behavior mismatch: %v (expected %v)", sigOk, expected)
+				}
+			})
+			t.Run(fmt.Sprintf("%s_Expanded/%d", n, idx), func(t *testing.T) {
+				if sigOk := tc.Run(t, implExpanded, opts); sigOk != expected {
 					t.Fatalf("behavior mismatch: %v (expected %v)", sigOk, expected)
 				}
 			})
