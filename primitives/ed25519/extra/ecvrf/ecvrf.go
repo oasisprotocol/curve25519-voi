2879729f5ee0d5166b6b288b9e3025aa775e7911
Yawning Angel <yawning@schwanenlied.me>
2022-07-07 09:48:57 +0000

    primitives/ed25519/extra/ecvrf: Bump to v13 of the IETF draft (BREAKING)
    Because the draft authors decided to add Y to the challenge generation
    input in v11, this breaks output compatibility.  If you need the
    previous semantics (for compatibility for example), use `Prove_v10`,
    `Verify_v10`, `ProveWithAddedRandomness_v10`.
    
    Note that with the v11 (and later) semantics, pi will change for a
    given key + message tuple, while beta will remain unchanged, thus
    there is only one `ProofToHash` routine.

--- primitives/ed25519/extra/ecvrf/ecvrf.go
+++ primitives/ed25519/extra/ecvrf/ecvrf.go
@@ -72,7 +72,16 @@ var (
 
 // Prove implements ECVRF_prove for the suite ECVRF-EDWARDS25519-SHA512-ELL2.
 func Prove(sk ed25519.PrivateKey, alphaString []byte) []byte {
-	piString, err := doProve(nil, sk, alphaString)
+	piString, err := doProve(nil, sk, alphaString, false)
+	if err != nil {
+		panic(err)
+	}
+	return piString
+}
+
+// Prove_v10 is Prove but using the v10 (and earlier) semantics.
+func Prove_v10(sk ed25519.PrivateKey, alphaString []byte) []byte {
+	piString, err := doProve(nil, sk, alphaString, true)
 	if err != nil {
 		panic(err)
 	}
@@ -89,10 +98,24 @@ func ProveWithAddedRandomness(rand io.Reader, sk ed25519.PrivateKey, alphaString
 	if rand == nil {
 		rand = cryptorand.Reader
 	}
-	return doProve(rand, sk, alphaString)
+	return doProve(rand, sk, alphaString, false)
 }
 
-func doProve(rand io.Reader, sk ed25519.PrivateKey, alphaString []byte) ([]byte, error) {
+// ProveWithAddedRandomness_v10 is ProveWithAddedRandomness but using the
+// v10 (and earlier) semantics.
+func ProveWithAddedRandomness_v10(rand io.Reader, sk ed25519.PrivateKey, alphaString []byte) ([]byte, error) {
+	if rand == nil {
+		rand = cryptorand.Reader
+	}
+	return doProve(rand, sk, alphaString, true)
+}
+
+func doProve(
+	rand io.Reader,
+	sk ed25519.PrivateKey,
+	alphaString []byte,
+	draftPreV11 bool,
+) ([]byte, error) {
 	// 1.  Use SK to derive the VRF secret scalar x and the VRF
 	// public key Y = x*B (this derivation depends on the ciphersuite,
 	// as per Section 5.5; these values can be cached, for example,
@@ -117,8 +140,8 @@ func doProve(rand io.Reader, sk ed25519.PrivateKey, alphaString []byte) ([]byte,
 	}
 	Y := sk[32:]
 
-	// 2.  H = ECVRF_hash_to_curve(Y, alpha_string)
-	H, err := hashToCurveH2cSuite(Y, alphaString)
+	// 2.  H = ECVRF_encode_to_curve(encode_to_curve_salt, alpha_string)
+	H, err := encodeToCurveH2cSuite(Y, alphaString)
 	if err != nil {
 		return nil, fmt.Errorf("ecvrf: failed to hash point to curve: %w", err)
 	}
@@ -159,11 +182,26 @@ func doProve(rand io.Reader, sk ed25519.PrivateKey, alphaString []byte) ([]byte,
 		return nil, fmt.Errorf("ecvrf: failed to deserialize k scalar: %w", err)
 	}
 
-	// 6.  c = ECVRF_hash_points(H, Gamma, k*B, k*H) (see Section 5.4.3)
-	var kB, kH curve.EdwardsPoint
+	// The challenge generation depends on the version of the IETF draft
+	// because they changed things as of draft v11 to include Y in the hash
+	// input.
+	//
+	// Old: c = ECVRF_hash_points(H, Gamma, k*B, k*H) (see Section 5.4.3)
+	// New: c = ECVRF_challenge_generation(Y, H, Gamma, k*B, k*H)
+	//
+	// Handle this in ECVRF_challenge_generation, since it is a matter
+	// of including Y or not.
+
+	var (
+		kB, kH curve.EdwardsPoint
+		p1     []byte
+	)
 	kB.MulBasepoint(curve.ED25519_BASEPOINT_TABLE, &k)
 	kH.Mul(H, &k)
-	c := hashPoints(&hString, &gammaString, &kB, &kH)
+	if !draftPreV11 {
+		p1 = Y
+	}
+	c := challengeGeneration(p1, &hString, &gammaString, &kB, &kH)
 
 	// 7.  s = (k + c*x) mod q
 	var s scalar.Scalar
@@ -208,9 +246,46 @@ func ProofToHash(piString []byte) ([]byte, error) {
 // The public key is validated such that the "full uniqueness" and
 // "full collision" properties are satisfied.
 func Verify(pk ed25519.PublicKey, piString, alphaString []byte) (bool, []byte) {
-	// 1.  D = ECVRF_decode_proof(pi_string) (see Section 5.4.4)
-	// 2.  If D is "INVALID", output "INVALID" and stop
-	// 3.  (Gamma, c, s) = D
+	return doVerify(pk, piString, alphaString, false)
+}
+
+// Verify_v10 is Verify but using the v10 (and earlier) semantics.
+func Verify_v10(pk ed25519.PublicKey, piString, alphaString []byte) (bool, []byte) {
+	return doVerify(pk, piString, alphaString, true)
+}
+
+func doVerify(
+	pk ed25519.PublicKey,
+	piString []byte,
+	alphaString []byte,
+	draftPreV11 bool,
+) (bool, []byte) {
+	var (
+		Y       curve.EdwardsPoint
+		yString curve.CompressedEdwardsY
+	)
+
+	// 1.   Y = string_to_point(PK_string)
+	if _, err := yString.SetBytes(pk); err != nil {
+		return false, nil
+	}
+	// 2.   If Y is "INVALID", output "INVALID" and stop
+	if !yString.IsCanonicalVartime() { // Required by RFC 8032 decode semantics.
+		return false, nil
+	}
+	if _, err := Y.SetCompressedY(&yString); err != nil {
+		return false, nil
+	}
+	// 3.   If validate_key, run ECVRF_validate_key(Y) (Section 5.4.5); if
+	//      it outputs "INVALID", output "INVALID" and stop
+	if Y.IsSmallOrder() { // Section 5.4.5 ECVRF Validate Key
+		// The IETF draft treats this as optional, but we always have enforced this.
+		return false, nil
+	}
+
+	// 4.   D = ECVRF_decode_proof(pi_string) (see Section 5.4.4)
+	// 5.   If D is "INVALID", output "INVALID" and stop
+	// 6.   (Gamma, c, s) = D
 	gamma, c, s, err := decodeProof(piString)
 	if err != nil {
 		return false, nil
@@ -218,36 +293,21 @@ func Verify(pk ed25519.PublicKey, piString, alphaString []byte) (bool, []byte) {
 	var gammaString curve.CompressedEdwardsY
 	_, _ = gammaString.SetBytes(piString[:32])
 
-	// 4.  H = ECVRF_hash_to_curve(Y, alpha_string)
-	var (
-		Y       curve.EdwardsPoint
-		yString curve.CompressedEdwardsY
-	)
-	if _, err := yString.SetBytes(pk); err != nil {
-		return false, nil
-	}
-	if !yString.IsCanonicalVartime() { // Required by RFC 8032 decode semantics.
-		return false, nil
-	}
-	if _, err := Y.SetCompressedY(&yString); err != nil {
-		return false, nil
-	}
-	if Y.IsSmallOrder() { // Section 5.6.1 ECVRF Validate Key
-		return false, nil
-	}
-	H, err := hashToCurveH2cSuite(yString[:], alphaString)
+	// 7.   H = ECVRF_encode_to_curve(encode_to_curve_salt, alpha_string)
+	//      (see Section 5.4.1)
+	H, err := encodeToCurveH2cSuite(yString[:], alphaString)
 	if err != nil {
 		panic("ecvrf: failed to hash point to curve: " + err.Error())
 	}
 	var hString curve.CompressedEdwardsY
 	hString.SetEdwardsPoint(H)
 
-	// 5.  U = s*B - c*Y
+	// 8.   U = s*B - c*Y
 	var U curve.EdwardsPoint
 	Y.Neg(&Y)
 	U.DoubleScalarMulBasepointVartime(c, &Y, s)
 
-	// 6.  V = s*H - c*Gamma
+	// 9.   V = s*H - c*Gamma
 	var V, negGamma curve.EdwardsPoint
 	negGamma.Neg(gamma)
 	V.MultiscalarMulVartime( // V = s*H + c*(-Gamma)
@@ -255,11 +315,20 @@ func Verify(pk ed25519.PublicKey, piString, alphaString []byte) (bool, []byte) {
 		[]*curve.EdwardsPoint{H, &negGamma},
 	)
 
-	// 7.  c' = ECVRF_hash_points(H, Gamma, U, V) (see Section 5.4.3)
-	cPrime := hashPoints(&hString, &gammaString, &U, &V)
+	// 10.  c' = ECVRF_challenge_generation(Y, H, Gamma, U, V) (see
+	//      Section 5.4.3)
+	//
+	// Note: Old (pre-v11) versions of the draft did not include Y,
+	// and instead did c' = ECVRF_hash_points(H, Gamma, U, V).
+	var p1 []byte
+	if !draftPreV11 {
+		p1 = pk[:]
+	}
+	cPrime := challengeGeneration(p1, &hString, &gammaString, &U, &V)
 
-	// 8.  If c and c' are equal, output ("VALID",
-	//     ECVRF_proof_to_hash(pi_string)); else output "INVALID"
+	// 11.  If c and c' are equal, output ("VALID",
+	//      ECVRF_proof_to_hash(pi_string)); else output "INVALID"
+
 	if c.Equal(cPrime) == 0 {
 		return false, nil
 	}
@@ -286,39 +355,49 @@ func gammaToHash(gamma *curve.EdwardsPoint) []byte {
 	return h.Sum(nil)
 }
 
-func hashToCurveH2cSuite(Y, alphaString []byte) (*curve.EdwardsPoint, error) {
-	// 1.  PK_string = point_to_string(Y)
-	// 2.  string_to_hash = PK_string || alpha_string
-	stringToHash := make([]byte, 0, len(Y)+len(alphaString))
-	stringToHash = append(stringToHash, Y...)
+func encodeToCurveH2cSuite(encodeToCurveSalt, alphaString []byte) (*curve.EdwardsPoint, error) {
+	// For the Edwards25519 curve:
+	// PK_string = point_to_string(Y)
+	// encode_to_curve_salt = PK_string
+
+	// 1. string_to_be_hashed = encode_to_curve_salt || alpha_string
+	stringToHash := make([]byte, 0, len(encodeToCurveSalt)+len(alphaString))
+	stringToHash = append(stringToHash, encodeToCurveSalt...)
 	stringToHash = append(stringToHash, alphaString...)
 
-	// 3.  H = encode(string_to_hash)
-	// 4.  Output H
+	// 2.  H = encode(string_to_hash)
+	// 3.  Output H
 	return h2c.Edwards25519_XMD_SHA512_ELL2_NU(h2cDST, stringToHash)
 }
 
-func hashPoints(p1, p2 *curve.CompressedEdwardsY, p3, p4 *curve.EdwardsPoint) *scalar.Scalar {
-	// 1.  two_string = 0x02 = int_to_string(2, 1), a single octet with
-	//     value 2
-	// 2.  Initialize str = suite_string || two_string
-	// 3.  for PJ in [P1, P2, ... PM]:
-	//       str = str || point_to_string(PJ)
-	// 4.  zero_string = 0x00 = int_to_string(0, 1), a single octet with
-	//     value 0
-	// 5.  str = str || zero_string
-	// 6.  c_string = Hash(str)
+func challengeGeneration(p1 []byte, p2, p3 *curve.CompressedEdwardsY, p4, p5 *curve.EdwardsPoint) *scalar.Scalar {
+	// 1.  challenge_generation_domain_separator_front = 0x02
+	// 2.  Initialize str = suite_string || challenge_generation_domain_separator_front
 	var (
 		tmp    curve.CompressedEdwardsY
 		digest [64]byte
 	)
 	h := sha512.New()
-	_, _ = h.Write([]byte{suiteString, twoString}) // suite_string || two_string
-	_, _ = h.Write(p1[:])                          // point_to_string(P1)
-	_, _ = h.Write(p2[:])                          // point_to_string(P2)
-	_, _ = h.Write(tmp.SetEdwardsPoint(p3)[:])     // point_to_string(P3)
-	_, _ = h.Write(tmp.SetEdwardsPoint(p4)[:])     // point_to_string(P4)
-	_, _ = h.Write([]byte{zeroString})             // zero_string
+	_, _ = h.Write([]byte{suiteString, twoString}) // suite_string || challenge_generation_domain_separator_front
+
+	// 3.  for PJ in [P1, P2, P3, P4, P5]:
+	//       str = str || point_to_string(PJ)
+	if len(p1) > 0 {
+		// This needs to handle both pre-v11 where Y was not included,
+		// and v11 and later, where Y is included.  This branch is the
+		// latter.
+		_, _ = h.Write(p1) // point_to_string(P1)
+	}
+	_, _ = h.Write(p2[:])                      // point_to_string(P2)
+	_, _ = h.Write(p3[:])                      // point_to_string(P3)
+	_, _ = h.Write(tmp.SetEdwardsPoint(p4)[:]) // point_to_string(P3)
+	_, _ = h.Write(tmp.SetEdwardsPoint(p5)[:]) // point_to_string(P4)
+
+	// 4.  challenge_generation_domain_separator_back = 0x00
+	// 5.  str = str || challenge_generation_domain_separator_back
+	_, _ = h.Write([]byte{zeroString}) // challenge_generation_domain_separator_back
+
+	// 6.  c_string = Hash(str)
 	h.Sum(digest[:0])
 
 	// 7.  truncated_c_string = c_string[0]...c_string[n-1]
